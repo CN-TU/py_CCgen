7d9270cbf11687cbd86b85a25369ea89b6e0ba6b
felixig <fiv@gmx.at>
2022-04-05 12:13:03 +0200

    Update tlsRlen.py

--- ccgen/techniques/tlsRlen.py
+++ ccgen/techniques/tlsRlen.py
@@ -9,17 +9,12 @@ class CovertChannel():
 	def modify(self, pkt, mappedvalue, params):
 		if(mappedvalue != -1):
 			load_layer("tls") 
-			print(pkt.haslayer(TLS),mappedvalue)
 			if pkt.haslayer(TLS):
-			    #print(pkt[TLS].show2())
-			    print(pkt[TLS][0].len)
 			    pkt[TLS][0].len = int(mappedvalue)+int(params['poff'])
-			    print(pkt[TLS][0].len)
 		return pkt
 
 	def extract(self, pkt, params):
 		if pkt.haslayer(TLS):
-			print(pkt[TLS][0].len)
 			return int(pkt[TLS][0].len)-int(params['poff'])
 
 
